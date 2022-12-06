a583474c69c0ddc52ebc0ca038fc1dddd33a39a2
Tomas Haas <tomas.haas@rossum.ai>
2022-12-06 22:27:44 +0100

    refactor day01

--- day01.hs
+++ day01.hs
@@ -5,11 +5,16 @@ import Data.List
 import Data.List.Split
 
 filename = "inputs/input01.txt"
-  
-elves1 = do  
-    contents <- readFile filename
-    return $ maximum $ map (sum . map read) $ splitOn [""] $ lines contents
 
-elves2 = do  
-    contents <- readFile filename
-    return $ sum $ take 3 $ reverse $ sort $ map (sum . map read) $ splitOn [""] $ lines contents
+main :: IO ()
+main = do
+    input <- readFile filename
+    let part1 = elves1 input
+        part2 = elves2 input
+    putStr $ show part1 <> " " <> show part2
+
+elves1 :: String -> Int  
+elves1 s = maximum $ map (sum . map read) $ splitOn [""] $ lines s
+
+elves2 :: String -> Int
+elves2 s = sum $ take 3 $ reverse $ sort $ map (sum . map read) $ splitOn [""] $ lines s
