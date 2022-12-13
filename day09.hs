381cc43eadae0d908fde6bfc4ea6722c193a49c0
Tomas Haas <tomas.haas@rossum.ai>
2022-12-13 21:14:10 +0100

    add day09 part2

--- day09.hs
+++ day09.hs
@@ -5,6 +5,7 @@ import qualified Data.Set as Set
 filename = "inputs/input09.txt"
 
 type Coord = (Int, Int)
+type Params = ([Coord], [Set.Set Coord])
 
 move :: String -> Coord -> Coord
 move "R" (x, y) = (x+1, y)
@@ -25,22 +26,28 @@ follow (x, y) (a, b) = let xa = x - a
                             | otherwise = b
                        in (a', b')
 
-doMove :: (Coord, Coord, Set.Set Coord) -> String -> (Coord, Coord, Set.Set Coord)
-doMove (head, tail, vs) d = let newHead = move d head
-                                newTail = follow newHead tail
-                                newVs = Set.insert newTail vs
-                            in (newHead, newTail, newVs)
+follow' :: [Coord] -> Int -> [Coord]
+follow' xs n = let before = take (n) xs
+                   new = follow (xs !! (n-1)) (xs !! n)
+                   after = drop (n+1) xs
+               in before ++ [new] ++ after
 
-iterMove :: (Coord, Coord, Set.Set Coord) -> String -> Int -> (Coord, Coord, Set.Set Coord)
-iterMove params d n = iterate (\x -> doMove x d) params !! n 
+move' :: Params -> String -> Params
+move' ((x:xs), vs) d = let moved = (move d x) : xs
+                           followed = foldl (\acc x -> follow' acc x) moved [1..(length moved)-1]
+                           vs1 = Set.insert (head followed) (head vs)
+                           vs9 = Set.insert (last followed) (last vs)
+                        in (followed, [vs1, vs9])
 
-get3rd :: (a, b, c) -> c
-get3rd (_,_,a) = a
+iterMove' :: Params -> String -> Int -> Params
+iterMove' params d n = iterate (\x -> move' x d) params !! n
 
 main :: IO ()
 main = do
     input <- readFile filename
     let moves = map (\x -> ((head x), (read (last x) :: Int))) $ map words $ lines input
-        go1 = foldl (\acc x -> iterMove acc (fst x) (snd x)) ((0,0), (0,0), Set.empty) moves
-        part1 = length $ get3rd go1
-    print $ part1
+        paramsFn n = (replicate n (0,0), replicate 2 Set.empty)
+        foldFn = (\acc x -> iterMove' acc (fst x) (snd x))
+        part1 = length $ last $ snd $ foldl foldFn (paramsFn 2) moves
+        part2 = length $ last $ snd $ foldl foldFn (paramsFn 10) moves
+    putStr $ show part1 <> " " <> show part2
